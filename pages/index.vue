44d570e4b8c14aed56be72023986362acd0279f9
ChristianPastenC <christian_pasten@outlook.com>
2022-06-07 17:20:35 -0500

    fix index html style

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,29 @@
 <template>
-  <div id="container" class="absolute text-white text-center custom-size w-full max-w-2xl px-6">
-    <h1 id="name" class="font-space-mono text-sm uppercase tracking-wide opacity-0"
-      style="transform: translateY(30px);">
+  <div
+    id="container"
+    class="absolute text-white text-center w-full max-w-2xl px-6"
+    style="top: 50%; transform: translate(-50%, -50%); left: 50%;"
+  >
+    <h1
+      id="name"
+      class="font-space-mono text-sm uppercase tracking-wide opacity-0"
+      style="transform: translateY(30px)"
+    >
       Christian Pasten
     </h1>
-    <p id="text" class="font-exo text-4xl opacity-0" style="transform: translateY(30px);">
+    <p
+      id="text"
+      class="font-exo text-4xl opacity-0"
+      style="transform: translateY(30px)"
+    >
       ONE WITH AN EVERLASTING DESIRE FOR THE UNKNOWN & UNTOLD
     </p>
-    <a href="https://github.com/ChristianPastenC" id="button"
+    <a
+      id="button"
+      href="https://chriscourses.com/"
       class="border px-4 py-2 rounded-lg text-sm font-space-mono uppercase mt-8 hover:bg-white hover:text-gray-800 inline-block opacity-0"
-      style="transform: translateY(30px);">
+      style="transform: translateY(30px)"
+    >
       View Work
     </a>
   </div>
