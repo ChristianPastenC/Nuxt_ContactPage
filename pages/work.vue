b3de7f1188e09292a8581ed2ce0e165fbda6a75b
ChristianPastenC <christian_pasten@outlook.com>
2022-06-08 16:10:01 -0500

    fix load bug

--- pages/work.vue
+++ pages/work.vue
@@ -1,11 +1,10 @@
 <template>
     <div class="bg-black h-screen px-6 py-12">
-        <h2 class="text-white font-exo text-5xl opacity-0" ref="title" style="transform: translateY(30px);">MY WORK</h2>
+        <h2 class="text-white font-exo text-5xl opacity-0" ref="title" style="transform: translateY(30px);">CONTACT</h2>
         <div class="flex space-x-4">
-            <div ref="projects" v-for="project in projects" class="w-full opacity-0"
-                style="transform: translateX(30px);">
+            <div ref="projects" v-for="project in projects" class="w-full opacity-0">
                 <nuxt-link to="/">
-                    <img :src="project.image.url" alt="" style="height: 70vh;" class="object-cover" />
+                    <img :src="project.image.url" style="height: 70vh;" class="object-cover" />
                 </nuxt-link>
                 <p class="text-white font-space-mono text-sm">{{ project.title }}</p>
             </div>
@@ -26,7 +25,6 @@
 <script>
 import imageB from "../assets/unsplash_b.jpg";
 import imageC from "../assets/unsplash_c.jpg";
-import imageD from "../assets/unsplash_d.jpg";
 import gsap from "gsap";
 export default {
     data() {
@@ -68,9 +66,8 @@ export default {
         });
         gsap.to(this.$refs.projects, {
             opacity: 1,
-            duration: 2,
+            duration: 1.5,
             stagger: 0.1,
-            x: 0,
             ease: 'expo',
             delay: 0.2,
         });
