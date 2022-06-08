22a258cd21b4c23a2758816406f930bdbc35515f
ChristianPastenC <christian_pasten@outlook.com>
2022-06-08 10:34:23 -0500

    customize work page style

--- pages/work.vue
+++ pages/work.vue
@@ -1,5 +1,15 @@
 <template>
-    <div>
-        <h2>WORK</h2>
+    <div class="bg-black h-screen px-6 py-12">
+        <h2 class="text-white font-exo text-5xl">MY WORK</h2>
     </div>
 </template>
+
+<style>
+.font-exo {
+    font-family: 'Exo 2', sans-serif;
+}
+
+.font-space-mono {
+    font-family: 'Space Mono', monospace;
+}
+</style>
